63e6acbc56837d6c82c6ed2dfdc90731bb1ee707
Chris White <chrisw@leehayes.com>
2017-08-10 11:45:24 -0400

    Reading open windows and tabs

--- src/view/view.js
+++ src/view/view.js
@@ -1,16 +1,55 @@
 //view.js
 
+var treeobj;
+
+/*
+function loadTree(tabarr)
+{
+    var windows={};
+
+    // Split the tabs into per-window groups
+    for(let tab in tabarr) {
+        if(!(tab.windowId in windows)) {
+            windows[tab.windowId] = {id: tab.windowId, name:'', tabs:[]};
+        }
+        windows[tab.windowId].push(tab);
+    }
+
+    // Fill in the per-window info
+    for(let win in windows) {
+        chrome.windows.get(win.id, {}, function(thewin) { });
+    }
+} //loadTree
+*/
+
+function loadTree(winarr)
+{
+    let dat = {};
+    for(let win of winarr) {
+        //console.log('Window');
+        let nodeid = treeobj.create_node(null, 'Window');
+        for(let tab of win.tabs) {
+            //console.log('   ' + tab.title);
+            treeobj.create_node(nodeid, tab.title);
+        }
+    } //foreach window
+} //loadTree(winarr)
+
 window.addEventListener('load',
     function() {
         console.log('TabFern view.js initializing jstree');
-        console.log($('#maintree').toString());
+        //console.log($('#maintree').toString());
         $('#maintree').jstree({ 'core': {
             'animation': false,
             'multiple': false,  // for now
+            'check_callback': true,  // for now, allow modifications
             themes: { 'name': 'default-dark' }
         }});
+        //chrome.tabs.query({}, loadTree);
+        treeobj = $('#maintree').jstree(true);
+        chrome.windows.getAll({'populate': true}, loadTree);
     },
     { 'once': true }
 );
 
-// vi: set ts=4 sts=4 sw=4 et ai fo-=o: //
+// vi: set ts=4 sts=4 sw=4 et ai fo-=or: //
