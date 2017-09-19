e929503417071e9e745973546b00ddf631c076f2
Chris White <chrisw@leehayes.com>
2017-09-19 08:24:52 -0400

    Pre-setup for tests [minor]

--- check-webstore.sh
+++ check-webstore.sh
@@ -17,7 +17,7 @@ echo === Files in master but not in webstore/ ===
 find . \( -wholename ./assets/icons -prune -o -name \*.swp -prune -o \
             -wholename ./webstore -prune -o -name .git\* -prune -o \
             -wholename ./scraps -prune -o -wholename ./webstore.js -prune -o \
-            -wholename ./webstore.zip -prune \) \
+            -wholename ./webstore.zip -prune -o -wholename ./test -prune \) \
     -o \( -type f \( -exec test -f webstore/{} \; -o -print \) \)
 
 cat <<EOF
---
+++ test/TODO.md
@@ -0,0 +1,6 @@
+ - Add a test link to the view menu if running unpacked - 
+   <https://stackoverflow.com/q/12830649/2877364>
+
+ - Add testcases here
+ - Add a testcase runner (Mocha?)
+
