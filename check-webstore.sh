6c705fc9f5423664bcee6cd65c72d73921ce00cb
Chris White <chrisw@leehayes.com>
2017-09-26 10:04:19 -0400

    Added package.json
    Getting a head start on #4 based on r4j4h's comments to date

--- check-webstore.sh
+++ check-webstore.sh
@@ -17,7 +17,8 @@ echo === Files in master but not in webstore/ ===
 find . \( -wholename ./assets/icons -prune -o -name \*.swp -prune -o \
             -wholename ./webstore -prune -o -name .git\* -prune -o \
             -wholename ./scraps -prune -o -wholename ./webstore.js -prune -o \
-            -wholename ./webstore.zip -prune -o -wholename ./test -prune \) \
+            -wholename ./webstore.zip -prune -o -wholename ./test -prune -o \
+            -wholename ./node_modules -prune \) \
     -o \( -type f \( -exec test -f webstore/{} \; -o -print \) \) | \
 grep -v '^./check-webstore.sh'
 
