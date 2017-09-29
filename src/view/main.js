4d604573c8b2f31e59d1c044455647257482b26c
Chris White <chrisw@leehayes.com>
2017-09-28 21:55:46 -0400

    Added another testcase

--- src/view/main.js
+++ src/view/main.js
@@ -2,10 +2,11 @@
 
 console.log({'main.js initial': chrome.runtime});
 
-// Comment out the following `let` => SUCCESS
-// Uncomment the `let` => FAILURE
+// Comment out the following `let` and `void` => SUCCESS
+// Uncomment the `let` or `void` => FAILURE
 
-//let W = window.frames[0];   // <======== THIS IS IT!!!!!!!!!!!!
+//let W = window.frames[0];
+//void window.frames[0];
 
 console.log({'main.js end': chrome.runtime});
 // vi: set ts=4 sts=4 sw=4 et ai fo-=o fo-=r: //
