ddcaa0399f8abf07f25236841468068146a12321
Chris White <chrisw@leehayes.com>
2017-09-29 10:24:29 -0400

    It works if you do window.frames[0] onload
    Apparently the issue is accessing window.frames[0] while the document is
    still loading.

--- woxxom/test.js
+++ woxxom/test.js
@@ -1,2 +1,20 @@
-//Comment the below line for success; uncomment for failure
+//Comment all of the four lines below for success; uncomment any for failure
+
+//console.log(window.frames[0]);
 //void window.frames[0];
+//void document.getElementsByTagName( "iframe" )[ 0 ].contentWindow;
+//window.setTimeout(goCrazy, 0);
+
+let W;
+
+function goCrazy(){ 
+    // Success, at least when this happens onload
+    void window.frames[0]; 
+    W = window.frames[0];
+    void document.getElementsByTagName( "iframe" )[ 0 ].contentWindow;
+    document.getElementById('message').textContent = 'loaded';
+}
+
+window.addEventListener('load', goCrazy, { 'once': true });
+
+// vi: set ts=4 sts=4 sw=4 et ai: //
