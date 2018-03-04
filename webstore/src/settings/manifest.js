fe18d95000c6a274d59eb5517bfb0b02539df851
Chris White <chrisw@leehayes.com>
2018-03-04 16:43:44 -0500

    Updated webstore for v0.1.16

--- webstore/src/settings/manifest.js
+++ webstore/src/settings/manifest.js
@@ -261,6 +261,19 @@ bar (it will start with "file://")</li>
             // Changelog                                          {{{1
             {
                 "tab": i18n.get("What's new?"),
+                "group": `Version 0.1.16${brplain('2018-xx-xx')}`,
+                'group_html':true,
+                "type": "description",
+                "text":
+`<ul>
+<li>The tree will now show the icons for the sites, even on closed windows.
+    Note that you may need to be online to see the icons.  ${issue(83)}</li>
+<li>TabFern won't ask you if you want to delete new tabs or about:blank tabs.
+    Hopefully this saves you a little bit of time.  ${issue(109)}</li>
+</ul>`
+            },
+            {
+                "tab": i18n.get("What's new?"),
                 "group": `Version 0.1.15${brplain('2018-02-09')}`,
                 'group_html':true,
                 "type": "description",
