5eaf14da3995f9f0f19a5c86dd6f513ee1c16db5
Chris White <chrisw@leehayes.com>
2018-09-21 14:52:06 -0400

    Correct runtime dependency to match brunch

--- app/win/main_tl.js
+++ app/win/main_tl.js
@@ -1,9 +1,9 @@
 // app/win/main.js
 
 // For some reason, brunch picks up the path-browserify that is part of
-// node-browser-modules rather than the one that is directly installed.
-// This alias takes care of that.  It is in this file because require.alias
-// is only defined at the top level (as far as I know).
+// node-browser-modules.  I think it is due to special handling in deppack.
+// This alias makes the bundled path available as 'path'.  It is in this file
+// because require.alias is only defined at the top level (as far as I know).
 require.alias("node-browser-modules/node_modules/path-browserify/index.js", "path");
 
 let Modules = require('win/deps');
