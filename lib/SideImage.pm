15d37f747eb75f24af90c1bdb9d01150a0901aac
Chris White <cxwembedded@gmail.com>
2019-11-03 22:13:28 -0500

    Plugin generates output
    Not the right output yet, but getting closer!

--- lib/SideImage.pm
+++ lib/SideImage.pm
@@ -11,37 +11,91 @@ BEGIN { $Data::Dumper::Indent = 1; }
 use Statocles::Base 'Class';
 with 'Statocles::Plugin';
 
+use Carp qw(croak);
+
 =head1 NAME
 
 SideImage - Put an image next to a block of text
 
 =head1 SYNOPSIS
 
-After adding this plugin to your site.yml, in a .markdown file:
+In C<site.yml>:
+
+    site:
+        args:
+            plugins:
+                side_image:
+                    $class: SideImage
+                    $args:
+                        template: site/whatever.html
+
+In a C<.markdown> file:
 
     % side_image 'image filename' => begin
     Markdown content to put next to the image
     % end
 
-The image filename will be passed to C<< $site->path(...) >>.
+The C<image filename> will be passed to C<< $site->url(...) >>.
+
+=head1 ATTRIBUTES
+
+=head2 template
+
+The template filename to use for rendering.  Relative to the theme.
+Does not include the C<.ep> extension.
+
+=cut
+
+has template => (
+    is => 'ro',
+    isa => Str,
+    required => 1
+);
 
 =head1 FUNCTIONS
 
 =head2 side_image
 
-Renders the content.
+Renders the content.  Passes to the L</template>:
+
+=over
+
+=item C<$image>
+
+The path to the specified image
+
+=item C<$content>
+
+The content for the left side
+
+=item Others
+
+C<$doc>, C<$app>, C<$site>, and C<$page>
+
+=back
 
 =cut
 
 sub side_image {
-    my ($self, $plugin_args, $img, $content) = @_;
-        # $plugin_args is a hashref with keys doc, app, site, page.
-    my $retval = $plugin_args->{site}->markdown->markdown(
-        #"Foo **bold** _italic_ bar\n```\nAnother paragraph\n```"
-        "<!-- side_image -->\n" . (Dumper(\@_) =~ s/^/    /gmr) .
-        "\n<!-- /side_image -->\n"
+    say STDERR 'side_image: ', Dumper(\@_);     # DEBUG
+    my ($self, $render_args, $img, $content) = @_;
+        # $render_args is a hashref with keys doc, app, site, page, ...
+
+    my $template = $render_args->{site}->theme->read($self->{template})
+        or croak "No template";
+    my $retval = $template->render(
+        image => $render_args->{site}->url($img),
+        content => $content,
+        map {; $_ => $render_args->{$_} } qw(app doc site page)
     );
-    #say STDERR $retval; # DEBUG
+    croak "No rendered output" unless defined $retval;
+
+    #my $retval = $render_args->{site}->markdown->markdown(
+    #    #"Foo **bold** _italic_ bar\n```\nAnother paragraph\n```"
+    #    "<!-- side_image -->\n" . (Dumper(\@_) =~ s/^/    /gmr) .
+    #    "\n<!-- /side_image -->\n"
+    #);
+    say STDERR $retval; # DEBUG
     return $retval;
 } #side_image()
 
