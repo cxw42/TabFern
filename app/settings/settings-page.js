f0a9b10333116161f7962a42a9c601dd5de26599
Chris White <cxwembedded@gmail.com>
2025-03-09 14:46:41 -0400

    WIP

--- app/settings/settings-page.js
+++ app/settings/settings-page.js
@@ -4,84 +4,79 @@
 /// Copyright (c) 2011 Frank Kohlhepp.
 /// SPDX-License-Identifier: MIT
 
-if(false) { // bundle these
-    require('vendor/common');
+if (false) {
+    // bundle these
+    require("vendor/common");
 }
 
-let $ = require('jquery');
+let $ = require("jquery");
 
-class Group
-{
+class Group {
     #$group;
 
-    constructor(parent, group_name)
-    {
+    constructor(parent, groupName) {
         this.#$group = $('<table class="setting group">');
         $(parent).append(this.#$group);
     }
 }
 
-class Tab
-{
+class Tab {
     #$tab;
     #$content;
     #groups = new Map();
 
-    constructor(tab_parent, content_parent, tab_name)
-    {
-        console.log({Adding_tab: tab_name});
+    constructor(tabParent, contentParent, tab_name) {
+        console.log({ Adding_tab: tab_name });
         this.#$tab = $('<div class="tab"></div>');
         this.#$tab.text(tab_name);
-        tab_parent.append(this.#$tab);
+        tabParent.append(this.#$tab);
 
         this.#$content = $('<div class="tab-content"/>');
-        this.#$content.append($('<h2>').text(tab_name));
-        content_parent.append(this.#$content);
+        this.#$content.append($("<h2>").text(tab_name));
+        contentParent.append(this.#$content);
     } // ctor
 
-    show()
-    {
-        this.#$tab.addClass('active');
-        this.#$content.addClass('show');
+    show() {
+        this.#$tab.addClass("active");
+        this.#$content.addClass("show");
     } // show()
 
-    addOrGetGroup(group_name)
-    {
+    addOrGetGroup(groupName) {
+        if (!this.#groups.has(groupName)) {
+        }
 
+        return this.#groups.get(groupName);
     } // addOrGetGroup()
-
 } // class Tab
 
-class SettingsPage
-{
-    #$parent;   // parent element, as jquery
+class SettingsPage {
+    #$parent; // parent element, as jquery
     #manifest;
     #tabs = new Map();
 
-    constructor(parent, manifest)
-    {
+    constructor(parent, manifest) {
         this.#$parent = $(parent);
         this.#manifest = manifest;
 
         this._createSkeleton(this.#$parent);
 
-        if(manifest.title) {
+        if (manifest.title) {
             this.#$parent[0].ownerDocument.title = manifest.title;
         }
-        if(manifest.label) {
-            $('#settings-label', this.#$parent).text(manifest.label);
+        if (manifest.label) {
+            $("#settings-label", this.#$parent).text(manifest.label);
         }
-        if(manifest.icon) {
-            $('#icon', this.#$parent).attr('src', manifest.icon);
+        if (manifest.icon) {
+            $("#icon", this.#$parent).attr("src", manifest.icon);
         }
 
-        for(const setting of (manifest.settings || [])) {
+        for (const setting of manifest.settings || []) {
             this._addSetting(setting);
         }
 
         // Show the first group
-        if(this.#tabs.size > 0) {
-            this.#tabs.values().next().value.show()
+        if (this.#tabs.size > 0) {
+            this.#tabs.values().next().value.show();
         }
     } // ctor
 
@@ -100,22 +95,22 @@ class SettingsPage
                     <h2 id="search-label"></h2>
                 </div>
             </div>
-        `)
+        `);
     }
 
     _addSetting(setting) {
-        console.log({Adding: setting});
+        console.log({ Adding: setting });
 
-        if(!this.#tabs.has(setting.tab)) {
-            this.#tabs.set(setting.tab, new Tab(
-                $('#tab-container'), $('#content'), setting.tab
-            ));
+        if (!this.#tabs.has(setting.tab)) {
+            this.#tabs.set(
+                setting.tab,
+                new Tab($("#tab-container"), $("#content"), setting.tab)
+            );
         }
         let tab = this.#tabs.get(setting.tab);
 
         group = tab.addOrGetGroup(setting.group);
     } // addSetting()
-
 } // class SettingsPage
 
 module.exports = SettingsPage;
